8964a2ec9d4e772a896ea5a1ec7fb0b6daa369cd
Ulrich Pogson <grapplerulrich@gmail.com>
2013-09-07 20:49:07 +0200

    Support shortcode in text widgets

--- class-flowplayer.php
+++ class-flowplayer.php
@@ -125,6 +125,9 @@ class Flowplayer5 {
 		// Add file support
 		add_filter( 'upload_mimes', array( $this, 'flowplayer_custom_mimes' ) );
 
+		// Allow shortcodes to work in text widgets
+		add_filter( 'widget_text', 'do_shortcode' );
+
 	}
 
 	/**
