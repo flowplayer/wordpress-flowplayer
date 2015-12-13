9af090b83f25c2ca20bce44faf65b7d742c99163
Ulrich Pogson <grapplerulrich@gmail.com>
2015-12-13 21:51:00 +0100

    Add new action under playlist markup
    - Remove actions within markup as they are not of much use

--- frontend/views/partials/playlist.php
+++ frontend/views/partials/playlist.php
@@ -1,10 +1,8 @@
 <div id="jsplaylist<?php echo absint( $playlist_id ); ?>" class="flowplayer-playlist flowplayer-playlist-<?php echo absint( $playlist_id ). ' ' . esc_attr( $playlist_options['fp5-select-skin'] ); ?>">
-	<?php do_action( 'fp5_playlist_top', $playlist_id, $atts ); ?>
 	<a class="fp-prev"><?php _e( '&lt; Prev', 'flowplayer5' ); ?></a>
 	<a class="fp-next"><?php _e( 'Next &gt;', 'flowplayer5' ); ?></a>
-	<?php do_action( 'fp5_playlist_bottom', $playlist_id, $atts ); ?>
 </div>
-
+<?php do_action( 'fp5_below_playlist', $playlist_id, $atts ); ?>
 <?php
 $current = current( $atts );
 if ( 'fp6' === $current['fp_version'] ) {
