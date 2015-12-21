ed41ce5b7463fa2954cd82a2de50fc4ae1189aaf
Ulrich Pogson <grapplerulrich@gmail.com>
2015-12-21 21:15:48 +0100

    Remove unneed var

--- frontend/views/partials/video-script.php
+++ frontend/views/partials/video-script.php
@@ -1,7 +1,7 @@
 <!-- Flowplayer single video config -->
 <script>
 jQuery( document ).ready( function( $ ) {
-	var fpVideo<?php echo absint( $atts['id'] ); ?> = $(".flowplayer-video-<?php echo esc_attr( $atts['id'] ); ?>").flowplayer(
+	fpVideo<?php echo absint( $atts['id'] ); ?> = $(".flowplayer-video-<?php echo esc_attr( $atts['id'] ); ?>").flowplayer(
 		<?php do_action( 'fp5_video_config', $atts['id'] ); ?>
 		<?php echo json_encode( $atts['js_config'] ); ?>
 	);
