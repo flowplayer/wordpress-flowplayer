beff00f26bc4e11d678c94d5594193f86dec790b
Ulrich Pogson <grapplerulrich@gmail.com>
2016-01-14 17:04:19 +0100

    Use the correct variable

--- frontend/views/partials/lightbox.php
+++ frontend/views/partials/lightbox.php
@@ -1,9 +1,9 @@
 <a href="#flowplayer-video-<?php echo absint( $atts['id'] ); ?>" class="open-popup-link">
 <?php
-	if ( 'link' == $atts['lightbox'] && isset( $return['video_data_config']['title'] ) ) {
-		echo esc_attr( $return['video_data_config']['title'] );
-	} elseif ( 'thumbnail' == $atts['lightbox'] && isset( $return['video_data_config']['title'] ) ) { ?>
-		<img src="<?php echo esc_url( $splash ); ?>" alt="<?php echo esc_attr( $return['video_data_config']['title'] ); ?>" height="<?php echo esc_attr( $atts['height'] ); ?>" width="<?php echo esc_attr( $atts['width'] ); ?>">
+	if ( 'link' == $atts['lightbox'] && isset( $atts['video_data_config']['title'] ) ) {
+		echo esc_attr( $atts['video_data_config']['title'] );
+	} elseif ( 'thumbnail' == $atts['lightbox'] && isset( $atts['video_data_config']['title'] ) ) { ?>
+		<img src="<?php echo esc_url( $splash ); ?>" alt="<?php echo esc_attr( $atts['video_data_config']['title'] ); ?>" height="<?php echo esc_attr( $atts['height'] ); ?>" width="<?php echo esc_attr( $atts['width'] ); ?>">
 	<?php } else {
 		_e( 'Click to open video', 'flowplayer5' );
 	} ?>
