da81d296b450bbd62f6fa4a9966a24df7dff97b1
Ulrich Pogson <grapplerulrich@gmail.com>
2016-01-18 11:30:50 +0100

    Remove action fp5_video_config

--- frontend/views/partials/video-script.php
+++ frontend/views/partials/video-script.php
@@ -2,7 +2,6 @@
 <script>
 jQuery( document ).ready( function( $ ) {
 	fpVideo<?php echo absint( $atts['id'] ); ?> = $(".flowplayer-video-<?php echo esc_attr( $atts['id'] ); ?>").flowplayer(
-		<?php do_action( 'fp5_video_config', $atts['id'] ); ?>
 		<?php echo json_encode( $atts['js_config'] ); ?>
 	);
 });
--- includes/deprecated.php
+++ includes/deprecated.php
@@ -25,6 +25,12 @@ function fp5_deprecated_hook_admin_notice(){
 		require_once( plugin_dir_path( FP5_PLUGIN_FILE ) . 'admin/includes/functions.php' );
 		echo caldera_warnings_dismissible_notice( $message, false, 'manage_options', 'fp5_filter_flowplayer_data' );
 	}
+
+	if ( has_filter( 'fp5_video_config' ) && is_admin() || defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		$message = __( 'The action <code>fp5_video_config</code> is being used. The action has been replaced by the filter <code>fp5_js_config</code>.', 'flowplayer5' );
+		require_once( plugin_dir_path( FP5_PLUGIN_FILE ) . 'admin/includes/functions.php' );
+		echo caldera_warnings_dismissible_notice( $message, false, 'manage_options', 'fp5_video_config' );
+	}
 }
 add_action( 'admin_notices', 'fp5_deprecated_hook_admin_notice' );
 
